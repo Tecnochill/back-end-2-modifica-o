843e0b270623e201e820e73a4ec7fa5ce370622c
Ágatha <agatha.rigotti@escola.pr.gov.br>
2026-06-01 11:27:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var AreaTotal = 9;
+if (AreaTotal > 10 ){
+    console.log("Area >=10");
+}else{
+    console.log("Area <=10");
+}
+
+document.getElementById("formulario").addEventListener("submit",function(event){
+
+
+});
